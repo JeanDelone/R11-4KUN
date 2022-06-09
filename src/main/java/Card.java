b2fbc0ba0157f2fe5f4a814e6074d07183094c22
unknown <jachu.mail@gmail.com>
2022-06-10 01:37:54 +0200

    created Card, creating board

--- src/main/java/Card.java
+++ src/main/java/Card.java
@@ -1,4 +1,8 @@
+import java.util.ArrayList;
+
 public class Card {
+
+    private String[] colors = {"RED", "BLUE", "BLACK", "YELLOW"};
 
     private boolean isJoker;
     private boolean isFirst;
@@ -12,13 +16,19 @@ public class Card {
         this.isJoker = isJoker;
     }
 
+
+    @Override
+    public String toString(){
+        return "(" + this.color + ", " + this.number + ")";
+    }
+
     @Override
     public boolean equals(Object o){
         if(o == this){
-            return true
+            return true;
         }
         if(!(o instanceof Card)){
-            return false
+            return false;
         }
 
         Card card = (Card)o;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,32 @@ public class Main {
 
     public static void main(String[] args){
 
+        Card[] board1 = {
+                new Card("red", 1, true, false),
+                new Card("red", 2, true, false),
+                new Card("red", 3, true, false),
+                new Card("red", 4, true, false),
+                new Card("red", 5, true, false),
+                new Card("red", 6, true, false),
 
-        System.out.println("essa");
+                new Card("blue", 1, true, false),
+                new Card("blue", 2, true, false),
+                new Card("blue", 3, true, false),
+
+                new Card("black", 1, true, false),
+                new Card("black", 2, true, false),
+                new Card("black", 3, true, false),
+        };
+
+        for(int i = 0; i < board1.length; i++){
+            System.out.print(board1[i]);
+        }
+
+        for (Card card : board1) {
+            System.out.println(card);
+        }
+
+
+
     }
 }
